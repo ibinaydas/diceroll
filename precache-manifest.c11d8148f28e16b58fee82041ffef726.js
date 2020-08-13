55d42ec40a68fd085e974e8fe2b9525ab0342d8d
Binay Das <ibinaydas@gmail.com>
2020-08-13 12:48:23 +0530

    Updates

---
+++ precache-manifest.c11d8148f28e16b58fee82041ffef726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df75d067398980dfbc9ef31baba5588",
+    "url": "/diceroll/index.html"
+  },
+  {
+    "revision": "6fd365991ca5de830651",
+    "url": "/diceroll/static/css/main.f13bfc2d.chunk.css"
+  },
+  {
+    "revision": "90330ab8084f8ace2865",
+    "url": "/diceroll/static/js/2.f8aaacf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diceroll/static/js/2.f8aaacf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd365991ca5de830651",
+    "url": "/diceroll/static/js/main.b541ec41.chunk.js"
+  },
+  {
+    "revision": "5a1ddbb4d9d11a4d2857",
+    "url": "/diceroll/static/js/runtime-main.f4827888.js"
+  }
+]);
